237fe982d3a4ee7773cdbc0bb52dbd6a7b45bbe2
‘Shelby <sls1881@gmail.com>
2021-03-22 15:35:31 -0700

    copy and paste push

--- copy-push.js
+++ copy-push.js
@@ -1,3 +1,7 @@
-const copyAndPush = (arr) => {
-return arr;
-}
+const copyAndPush = (arr, item) => {
+  const result = arr;
+  arr.push(item);
+  return result;
+};
+
+module.exports = copyAndPush;
--- copy-push.test.js
+++ copy-push.test.js
@@ -1,11 +1,16 @@
+const copyAndPush = require('./copy-push.js');
 // describe('', () => {
 //     it('', () => {
-    
+
 //     });
 // });
 
 describe('copy and push function', () => {
-    it('copies an array and pushes a new object to it', () => {
-    
-    });
+  it('copies an array and pushes a new object to it', () => {
+    const numbers = [1, 2, 3];
+    const newArr = copyAndPush(numbers, 4);
+    const finalArr = [1, 2, 3, 4];
+
+    expect(newArr).toEqual(finalArr);
+  });
 });
